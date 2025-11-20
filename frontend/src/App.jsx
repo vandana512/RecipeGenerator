b5bf701b1b5aa927e46c2dc4eb11449841bab211
Vandana <vandanaaa512@gmail.com>
2025-11-20 15:11:00 +0530

    feat: add Home page to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import { RecipeProvider } from './context/RecipeContext';
+import Home from './pages/Home';
 
+function App() {
+  return (
+    <RecipeProvider>
+      <Home />
+    </RecipeProvider>
+  );
+}
+
+export default App;
